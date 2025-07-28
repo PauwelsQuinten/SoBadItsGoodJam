6117cb50c50e0ec9e8400ab45656f709c5d1fdbb
PauwelsQuinten <pauwels.quinten2.0@gmail.com>
2025-07-28 13:38:19 +0200

    making the scratching mechanic

--- Assets/Scripts/Utility/CustomEventArgs/ValveRotationChangedEventArgs.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-public class ValveRotationChangedEventArgs : EventArgs
-{
-    public float ValveRotation;
-    public GameObject Valve;
-}
--- Assets/Scripts/Utility/CustomEventArgs/WasteTimerChangedEventArgs.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using UnityEngine;
-
-public class WasteTimerChangedEventArgs : EventArgs
-{
-    public int WasteTimer;
-}
